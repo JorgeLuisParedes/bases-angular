fe142373e14ea7f897601a616073504faa5a0973
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-03-10 23:53:19 +0100

    formmodule y ngmodel

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'dbz-add-character',
-  templateUrl: './add-character.component.html',
-  styleUrl: './add-character.component.css'
+	selector: 'dbz-add-character',
+	templateUrl: './add-character.component.html',
+	styleUrl: './add-character.component.css'
 })
 export class AddCharacterComponent {
+	public charater: Character = {
+		name: '',
+		power: 0
+	};
 
+	emitCharacter() {
+		console.log(this.charater);
+
+		this.charater.name = '';
+		this.charater.power = 0;
+	}
 }
